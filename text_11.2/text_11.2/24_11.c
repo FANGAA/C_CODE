5202d3300b8c351b467bee128be5ad6f145866c3
fang-aa <3581849139@qq.com>
2024-11-02 16:27:12 +0800

    today

---
+++ text_11.2/text_11.2/24_11.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	/*'a';
+	'q';
+	'A';*/
+	//32-127
+	//int i = 0;
+	//for (i = 32; i <= 127; i++)
+	//{
+	//	printf("%c ", i);
+	//	if (i % 16 == 15)//»»ÐÐ
+	//		printf("\n");
+	//}
+	//return 0;
+
+		printf("%d\n", strlen("c:\test\121"));
+		return 0;
+	
+}
